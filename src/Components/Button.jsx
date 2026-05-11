d21c48ba0d2841bbd0470cd20006714bf6efd855
khalilsediq <khalilullahsadique1334@gmail.com>
2026-05-11 17:12:45 +0500

    first commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    
+    <>
+    <Button>Add Counter</Button>
+    </>
+  )
+}
+
+export default Button
---
+++ src/Components/Compo1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Compo1 = () => {
+  return (
+    <div>Compo1</div>
+  )
+}
+
+export default Compo1
